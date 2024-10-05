74b2c55b7e47c6b558425c60d60a36b173d61eb1
hyans221 <thyha221@gmail.com>
2024-10-05 15:53:42 +0700

    BT Module 2

---
+++ Module2/restaurantMenu/restaurant_menu.js
@@ -0,0 +1,28 @@
+// const breakfastMenu  = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu  = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu  = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index+1}:${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index+1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+
+// ----------------- Method 1
+// let dessertItem = '';
+// dessertMenu.forEach((item, index) => {
+//     dessertItem += `<p>Item ${index+1}: ${item}</p>`;
+// });
+// document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+
+// ----------------- Method 2
+let dessertItem = '';
+for(let i = 0; i <= dessertMenu.length; i++)
+{
+    dessertItem += `<p>Item ${i+1}: ${dessertMenu[i]}</p>`;
+}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
