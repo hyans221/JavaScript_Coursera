c04c886222829538d368e3b1668ef791ef858e2d
hyans221 <thyha221@gmail.com>
2024-10-06 23:25:35 +0700

    bt

---
+++ Module2/bookSystem/book_system.js
@@ -0,0 +1,44 @@
+let books = [];
+
+function addBook() {
+    const bookName = document.getElementById('bookName').value;
+    const authorName = document.getElementById('authorName').value;
+    const bookDescription = document.getElementById('bookDescription').value;
+    const pagesNumber = parseInt(document.getElementById('pagesNumber').value);
+
+    if (bookName && authorName && bookDescription && !isNaN(pagesNumber)) {
+        const book = {
+            name: authorName,
+            authorName: authorName,
+            bookDescription: bookDescription,
+            pagesNumber: pagesNumber
+        };
+        books.push(book);
+        showbooks();
+        clearInputs();
+    }
+    else {
+        alert('Vui lòng điền tất cả các trường.');
+    }
+}
+
+
+function showbooks() {
+    const displayBooks = books.map((item, index) => `<h1>Book number: ${index + 1}</h1>
+    <p>Book Name: ${item.bookName}</p>
+    <p>Author Name: ${item.authorName}</p>
+    <p>Book Description: ${item.bookDescription}</p>
+    <p>No. of Pages Time: ${item.pagesNumber}</p>`
+    ).join('');
+
+    document.getElementById('books').innerHTML = displayBooks;
+}
+
+
+function clearInputs()
+{
+    document.getElementById('bookName').value = '';
+    document.getElementById('authorName').value = '';
+    document.getElementById('bookDescription').value = '';
+    document.getElementById('pagesNumber').value = '';
+}
