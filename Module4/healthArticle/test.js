945d12ffbc4b4f7a32af87d32a826c07c1a40023
hyans221 <thyha221@gmail.com>
2024-10-13 23:10:54 +0700

    up

--- Module4/healthArticle/test.js
+++ Module4/healthArticle/test.js
@@ -1,10 +1,13 @@
 var xhr = new XMLHttpRequest();
 var url = './health_article.json';
+
 xhr.open('GET', url, true);
 xhr.responseType = 'json';
 
 var articles = xhr.response.articles;
 var articlesDiv = document.getElementById('articles');
+
+
 
 articles.forEach(function (article) {
     var articleDiv = document.createElement('div');
